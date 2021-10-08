c70e963deafe71cef5c05bdd6473d5858d926f54
nikhiltripathi1 <tripathin645@gmail.com>
2021-10-08 18:23:14 +0530

    'updated'

--- server/constants.js
+++ server/constants.js
@@ -1,9 +1,8 @@
 const uri =
-  "mongodb+srv://root:kNvsHKtIKRdAUXr0@cluster0.vnrat.mongodb.net/inventory?retryWrites=true&w=majority";
+  "mongodb+srv://admin:M0VCui1WSKfYi5eT@cluster0.rnzw0.mongodb.net/property?retryWrites=true&w=majority";
 const jwtSecret = "Shhh!@ItsaJwtSeceret";
 module.exports = {
   uri: uri,
   jwtSecret: jwtSecret,
 };
-
-//BNWHR6AQe3zwR9D
+//M0VCui1WSKfYi5eT
